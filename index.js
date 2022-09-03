cc1deb76c4d99b17fbafcc5f3da8f1887e01d532
karzan <karzanluqman99@gmail.com>
2022-09-03 03:06:11 +0300

    not done tho

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your code here!
+
+//const element = document.createElement("div");
+const p = document.createElement("p");
+//document.body.append(element);
+
+const newHeader = document.createElement("h1");
+
+document.getElementById("main").remove();
+console.log("first")
+p.style.color = "red";
+newHeader.innerHTML = "is the champion"
+document.body.append(newHeader);
+document.body.append(p);
+p.innerHTML = "hello world"
+newHeader.setAttribute("id", "victory")
+
+
+//document.body.removeChild("main");
+
+
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+// }
+
+// element.append(ul);
+
+// const ull = document.getElementsById("main")
+
+// document.removeChild(ull);
+// const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
