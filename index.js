fa585cff770a6f32d3630db6396d91ba3eb4edc8
karzan <karzanluqman99@gmail.com>
2022-09-03 10:54:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const newHeader = document.createElement("h1");
 document.getElementById("main").remove();
 console.log("first")
 p.style.color = "red";
+//newHeader.id = "victory";
+newHeader.setAttribute("id", "victory")
+
 newHeader.innerHTML = "is the champion"
 document.body.append(newHeader);
 document.body.append(p);
 p.innerHTML = "hello world"
-newHeader.setAttribute("id", "victory")
-
 
 //document.body.removeChild("main");
 
